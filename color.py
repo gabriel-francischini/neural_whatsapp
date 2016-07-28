be165704181b032c24c621199af4c7766fe33f6b
Gabriel F <empresagabriel@gmail.com>
2016-07-27 22:21:40 -0300

    Rewrote and commented color.py

--- color.py
+++ color.py
@@ -1,49 +1,68 @@
 #!/usr/bin/python
 # -*- coding: utf-8
+from os import isatty
 from colorama import *
 
+# Initialize colorama, important for windows
+init()
 
-def color(string, color):
-	init()
-	return (color + string + Fore.RESET + Back.RESET + Style.RESET_ALL)
+# This is a function for easly coloring output
+def colorize(string, *colors):
+	
+	# If script is running on a console,
+	# color information should be added
+	if isatty():
 
-def pcolor(scolor,string):
-	try:
-		try:
-			try:
-				try:
-					print(color(string,scolor))
-				except UnicodeError:
-					print(color(string.encode("latin-1", "ignore"), scolor))
-			except UnicodeError:
-				print(color(string.encode("ascii", "ignore"),scolor))
-		except UnicodeError:
-			print("\nError while printing a string".encode("ascii", "ignore"))
-	except:
-		pass
-	#try:
-	#	print(color(string,scolor))
-	#except UnicodeError:
-	#	try:
-	#		print(color(string.encode("latin-1", "ignore"), scolor))
-	#	except UnicodeError:
-	#		try:
-	#			print(color(string.encode("ascii", "ignore"),scolor))
-	#		except UnicodeError:
-	#			try:
-	#				print("\nError while printing a string".encode("ascii", "ignore"))
-	#			except UnicodeError:
-	#				pass
-	finally:
-		return None
+		# colored_string is a self-contained colored string
+		colored_string = ""
+
+		# User's options about color and style are added to string...
+		for color in colors:
+			colored_string += color
 
 
-def colorChoice(evaluation, colorFalse = Fore.RED, colorTrue = Fore.GREEN):
-	if evaluation:
-		return colorTrue
+		# And the we prevent other non-related strings 
+		# to be formatted as above
+		colored_string += Fore.RESET + Back.RESET + Style.RESET_ALL
+	
 	else:
-		return colorFalse
+		# Otherwise, string show be a raw string
+		colored_string = string
+
+	# Frequently UnicodeEncodeError or UnicodeDecodeError
+	# rises while attempting to print out the string
+	try:
+		print(colored_string)
+	
+	# If it do rise, the we should try other encoding
+	except UnicodeError:
+		try:
+			print(colored_string.encode("latin-1", "ignore"))
+		
+		except UnicodeError:
+			try:
+				print(colored_string.encode("ascii", "ignore"))
+			
+			# At last, when no solution worked, we output a raw string
+			except UnicodeError:
+				try:
+					print(string)
+
+				# May God prevent us from needing to ignore the
+				# original string and print out something else
+				except UnicodeError:
+					print("\nFatal error while printing string.")
 
 
-def colorGen(string, value):
-	return color(string, colorChoice(value > 0))
+	return None
+
+
+# A coloring function based in some evaluation
+def colorChoice(string, evaluation, colorTrue = Fore.GREEN, colorFalse = Fore.RED):
+	if evaluation:
+		colorize(string, colorTrue)
+	
+	else:
+		colorize(string, colorFalse)
+
+	return
